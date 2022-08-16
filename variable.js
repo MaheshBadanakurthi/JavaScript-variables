581979f87135f364d218addf9f28173e83e2836c
Mahesh <itstribe.mahi@gmail.com>
2022-08-16 15:44:17 +0530

    variable declaring and getting

---
+++ variable.js
@@ -0,0 +1,27 @@
+//   abseve lines of code
+// x='mahesh';
+// var x=109;
+//console.log(x);
+
+// const n="cgcs";
+//  n='CGCS';
+//console.log(n);
+
+// var y='siva';
+// console.log(y);
+// 
+
+// constant used in this
+// var age=24;
+// age=25;
+// const byear=1997;
+// console.log(age);
+// console.log(byear);
+
+// not defined
+
+// var n;
+// var x=null;
+// console.log(n)
+// console.log(x)
+///
